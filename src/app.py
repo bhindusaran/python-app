f9e213406d75d07a7a0d1e296f58b899bf54dd27
bhindusaran <bhindusaran@gmail.com>
2025-08-31 16:02:05 +0530

    uses self hosted runner

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/api/v1/details')
 def details():
         return jsonify({
-                'message': 'hello_world. you are doing great, human',
+                'message': 'hello_world. you are doing great, human !',
                 'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
                 'hostname': socket.gethostname() 
         })
